fb64f52fa279c28a9f49015b247776afec31abe0
Gilles FRANCOIS <gilles@lesfrancois.com>
2024-03-19 11:21:48 +0100

    Update server-script.sh

--- server-script.sh
+++ server-script.sh
@@ -43,25 +43,13 @@ DEST_FOLDER="/home/$USERNAME/$DEST_FOLDERNAME"
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 ###########################################################################
 echo "###### ###### Apache"
-#!/bin/bash
 
 # Check if Apache is installed (apache2 for Debian/Ubuntu, httpd for Red Hat/CentOS)
 if ! command -v apache2 >/dev/null 2>&1 && ! command -v httpd >/dev/null 2>&1; then
     echo "Apache is not installed. Installing..."
 
-    # Detect the package management system and install Apache
-    if [ -f /etc/debian_version ]; then
-        # Debian-based distributions (Debian, Ubuntu, etc.)
-        sudo apt update
-        sudo apt install apache2 -y
-    elif [ -f /etc/redhat-release ]; then
-        # Red Hat-based distributions (RHEL, CentOS, Fedora, etc.)
-        sudo yum update
-        sudo yum install httpd -y
-    else
-        echo "Unsupported Linux distribution."
-        exit 1
-    fi
+    apt update
+    apt install apache2 -y
 
     ufw allow 'Apache'
     ufw allow OpenSSH
@@ -69,10 +57,80 @@ if ! command -v apache2 >/dev/null 2>&1 && ! command -v httpd >/dev/null 2>&1; t
     echo "Apache has been installed successfully."
 else
     echo "Apache is already installed."
-    systemctl status apache2    
 fi
 
+###########################################################################
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+###########################################################################
+echo "###### ###### Domain"
 
+# Apache document root
+DOC_ROOT="/var/www/html"
+
+# Generate a random file name
+TEMP_FILE="tempfile_$SERVER_NAME.html"
+
+# Create the temporary file in the document root
+echo "This is a temporary file" > "$DOC_ROOT/$TEMP_FILE"
+
+URL="https://tools.sopress.net/wget/?h=true&u=https://$SERVER_NAME/$TEMP_FILE"
+echo $URL
+if curl --silent "$URL" | grep -q "404"; then
+    echo "The domain $SERVER_NAME is not serving content from this server."
+
+    read -p "Do you want to continue (Y|n)? " -n 1 -r REPLY
+    echo    # Move to a new line
+
+    # Set default value if no input is given
+    if [[ -z "$REPLY" ]]; then
+        REPLY='Y'
+    fi
+
+    # Check the reply
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo "Proceeding..."
+        # Place your script logic here that executes if the user agrees to continue
+    else
+        echo "Exiting..."
+        exit 1
+        # The script will exit if the user doesn't agree to continue
+    fi    
+else
+    echo "The domain $SERVER_NAME is serving content from this server."
+fi
+
+# Clean up: Remove the temporary file
+rm -f "$DOC_ROOT/$TEMP_FILE"
+
+###########################################################################
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+###########################################################################
+echo "###### ###### MySQL"
+if ! which mysql >/dev/null; then
+  echo "Installing MySQL..."
+  apt update -qq  > /dev/null 2>&1
+  apt install mysql-server -y -qq  > /dev/null 2>&1
+  read -p "MySQL password : " MYSQL_PASSWORD
+
+  QUERY="ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_PASSWORD';"
+  QUERY="$QUERY;CREATE USER '$USERNAME'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+  QUERY="$QUERY;GRANT ALL PRIVILEGES ON *.* TO '$USERNAME'@'%';"
+  QUERY="$QUERY;FLUSH PRIVILEGES;"
+
+
+  mysql -u root -p"$MYSQL_PASSWORD" -e "$QUERY"
+
+
+  CONFIG_FILE="/etc/mysql/mysql.conf.d/mysqld.cnf"
+  # Replace bind-address value from 127.0.0.1 to 0.0.0.0
+  sed -i '0,/bind-address\s*=\s*127\.0\.0\.1/s//bind-address            = 0.0.0.0/' "$CONFIG_FILE"
+  echo "bind-address has been updated."
+
+  systemctl restart mysql
+  # mysql_secure_installation
+else
+  echo "MySQL is already installed."
+fi
 ###########################################################################
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 ###########################################################################
@@ -107,7 +165,7 @@ sudo hostnamectl set-hostname $SERVER_NAME
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 ###########################################################################
 echo "###### ###### User $USERNAME"
-adduser $USERNAME
+adduser $USERNAME >/dev/null 2>&1 
 
 usermod -aG sudo $USERNAME
 
@@ -124,7 +182,7 @@ cp $SUDOERS_FILE "$SUDOERS_FILE.bak"
 sed 's/ALL=(ALL:ALL) ALL/ALL=(ALL:ALL) NOPASSWD:ALL/' "$SUDOERS_FILE.bak" > $TMP_FILE
 
 # Check syntax of the new sudoers file
-visudo -c -f $TMP_FILE
+visudo -c -f $TMP_FILE >/dev/null 2>&1 
 if [ $? -eq 0 ]; then
     # Syntax is okay, replace the original file
     cp $TMP_FILE $SUDOERS_FILE
@@ -189,4 +247,4 @@ if [ ! -f "$DEST_FOLDER" ]; then
   echo "Depot already cloned"
 else
   sudo -u "$USERNAME" GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone "$GIT" "$DEST_FOLDER"
-fi
+fi  
